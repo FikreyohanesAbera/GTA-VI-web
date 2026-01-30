898bddfe8cf212ad512f30364dbccaf6f4fbfaa7
Fikreyohanes Abera <godfathershemsu@gmail.com>
2026-01-30 11:47:49 +0300

    first initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1></h1>
+      </div>
+    </main>
+  )
+
+
+
+}
+
+export default App;
