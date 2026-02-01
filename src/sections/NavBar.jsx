4c614049801b15da3a1ea537ede9702e98c25a3c
Fikreyohanes Abera <godfathershemsu@gmail.com>
2026-02-01 22:25:33 +0300

    first page

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -3,6 +3,7 @@ const NavBar = () => {
         <nav>
           <img src="/images/nav-logo.svg" className='scale-90' alt="" />
           <img src="/images/menu.svg" className='w-10' alt="" />
+          
         </nav>
     )
 }
